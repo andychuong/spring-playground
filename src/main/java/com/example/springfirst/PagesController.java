673a995ecf17bf5d95a052e06eb35e8a5b593b8c
Andy Chuong <achuong55@gmail.com>
2018-11-15 10:48:54 -0700

    pagesController

--- src/main/java/com/example/springfirst/PagesController.java
+++ src/main/java/com/example/springfirst/PagesController.java
@@ -4,11 +4,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class HelloController {
+public class PagesController {
 
-    @GetMapping("/")
-    public String helloWorld() {
-        return "Hello World!";
+    @GetMapping("/hello")
+    public String sayHello(){
+
+        return "Hello World";
     }
-
 }
